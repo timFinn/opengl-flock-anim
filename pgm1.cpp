919a0f736dc6be575bd17131871568dc4fe8ba63
timFinn <polyphonictimothy@gmail.com>
2018-08-27 21:02:29 -0500

    added timer to animate flock

--- pgm1.cpp
+++ pgm1.cpp
@@ -1,11 +1,14 @@
 #include <GL/glut.h>
 #include <stdio.h>
+#include <iostream>
 
 #include "my_setup_3D_18.h"
 
 #define canvas_Width 640
 #define canvas_Height 480
 #define canvas_Name "CS 445 - Program 1"
+
+int x_offset;
 
 void drawGoose(int init_x, int init_y)
 {
@@ -22,21 +25,54 @@ void drawGoose(int init_x, int init_y)
     glEnd();
 }
 
+void drawClouds()
+{
+    
+}
+
 void updatePosition()
 {
     //Code logic for taking value pass in using pointer and update the position
     //values of the flock members every 75 - 100ms
+    ++x_offset;
+    glutPostRedisplay();
+}
+
+void kbHandler(unsigned char key, int x, int y)
+{
+    std::cout << key << std::endl;
+    // switch(key)
+    // {
+    //     case 32:
+    //         printf("Start flying");
+    //         break;
+    //     case 88:
+    //         exit(0);
+    //         break;
+    //     default:
+    //         break;
+    // }
+
 }
 
 void display_func()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    drawGoose(190, (canvas_Height / 2));
-    drawGoose(50, (canvas_Height / 2) + 75);
-    drawGoose(50, (canvas_Height / 2) - 75);
+    drawGoose(190 + x_offset, (canvas_Height / 2));
+    drawGoose(50 + x_offset, (canvas_Height / 2) + 75);
+    drawGoose(50 + x_offset, (canvas_Height / 2) - 75);
+
+    //updatePosition();
 
     glFlush();
+}
+
+void timer_func(int ID)
+{
+    ++x_offset;
+    display_func();
+    glutTimerFunc(75, timer_func, 1);
 }
 
 void initRendering()
@@ -45,31 +81,19 @@ void initRendering()
     glLineWidth(2.0);
 }
 
-void kbHandler(unsigned char key, int x, int y)
-{
-    switch(key)
-    {
-        case 32:
-            printf("Start flying");
-            break;
-        case 88:
-            exit(0);
-            break;
-        default:
-            break;
-    }
-
-}
-
 int main (int argc, char ** argv)
 {
-    glutInit(&argc, argv);
+    x_offset = 0;
 
+    glutInit(&argc, argv);
+    glutKeyboardFunc(kbHandler);
     my_setup(canvas_Width, canvas_Height, canvas_Name);
     initRendering();
+    
+    glutTimerFunc(75, timer_func, 1);
 
     glutDisplayFunc(display_func);
-    glutKeyboardFunc(kbHandler);
+    
 
     glutMainLoop();
     return 0;
